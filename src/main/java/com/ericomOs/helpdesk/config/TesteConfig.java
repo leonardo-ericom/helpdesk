caf6593297ce7171127047b76dae29338b620c65
leonardo-ericom <leonardo@ericom.com.br>
2023-05-08 08:20:37 -0300

    perfil de desenvolvimento

---
+++ src/main/java/com/ericomOs/helpdesk/config/TesteConfig.java
@@ -0,0 +1,22 @@
+package com.ericomOs.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ericomOs.helpdesk.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TesteConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void intanciaDB() {
+		this.dbService.instanciaDB();
+	}
+
+}

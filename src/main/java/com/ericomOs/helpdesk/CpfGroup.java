e0cfe458d175cd6e42837eab4e3b1c0996e55c42
leonardo-ericom <leonardo@ericom.com.br>
2023-05-09 17:54:42 -0300

    crud para cliente implementado

--- src/main/java/com/ericomOs/helpdesk/CpfGroup.java
+++
@@ -1,5 +0,0 @@
-package com.ericomOs.helpdesk;
-
-public interface CpfGroup {
-
-}
--- src/main/java/com/ericomOs/helpdesk/domain/enums/Perfil.java
+++ src/main/java/com/ericomOs/helpdesk/domain/enums/Perfil.java
@@ -2,7 +2,7 @@ package com.ericomOs.helpdesk.domain.enums;
 
 public enum Perfil {
 
-	ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO(3, "ROLE_TECNICO");
+	ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO(2, "ROLE_TECNICO");
 
 	private Integer codigo;
 	private String descricao;

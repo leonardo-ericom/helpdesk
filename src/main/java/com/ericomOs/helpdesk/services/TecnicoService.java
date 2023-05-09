99275dd1357a2143976e46516ec236eda30597ec
leonardo-ericom <leonardo@ericom.com.br>
2023-05-09 10:06:33 -0300

    Endpoint created para tecnico

--- src/main/java/com/ericomOs/helpdesk/services/TecnicoService.java
+++ src/main/java/com/ericomOs/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ericomOs.helpdesk.domain.Tecnico;
+import com.ericomOs.helpdesk.domain.dtos.TecnicoDTO;
 import com.ericomOs.helpdesk.repositories.TecnicoRepository;
 import com.ericomOs.helpdesk.services.exception.ObjectnotFoundException;
 
@@ -25,5 +26,11 @@ public class TecnicoService {
 	public List<Tecnico> findAll() {
 
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }

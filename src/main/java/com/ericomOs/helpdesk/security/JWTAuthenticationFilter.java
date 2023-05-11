183dc9522fd0d23c4ec148f124a2ba9826cf6cbb
leonardo-ericom <leonardo@ericom.com.br>
2023-05-11 16:28:20 -0300

    autenticacao com JWT

---
+++ src/main/java/com/ericomOs/helpdesk/security/JWTAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package com.ericomOs.helpdesk.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.ericomOs.helpdesk.domain.dtos.CredenciaisDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager autenAuthenticationManager;
+	private JWTUtil jwtUtil;
+
+	public JWTAuthenticationFilter(AuthenticationManager autenAuthenticationManager, JWTUtil jwtUtil) {
+		super();
+		this.autenAuthenticationManager = autenAuthenticationManager;
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		try {
+			CredenciaisDTO creds = new ObjectMapper().readValue(request.getInputStream(), CredenciaisDTO.class);
+			UsernamePasswordAuthenticationToken autenAuthenticationToken = new UsernamePasswordAuthenticationToken(
+					creds.getEmail(), creds.getSenha(), new ArrayList<>());
+			Authentication authentication = autenAuthenticationManager.authenticate(autenAuthenticationToken);
+			return authentication;
+
+		} catch (Exception e) {
+			throw new RuntimeException(e); // TODO: handle exception
+		}
+
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		String username = ((UserSS) authResult.getPrincipal()).getUsername();
+		String token = jwtUtil.generateTOken(username);
+
+		response.setHeader("accesss-control-expose-headers", "Authorization");
+		response.setHeader("Authorization", "Bearer" + token);
+
+	}
+
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+
+		response.setStatus(401);
+		response.setContentType("application/json");
+		response.getWriter().append(json());
+
+	}
+
+	private CharSequence json() {
+		long date = new Date().getTime();
+		return "{" + "\"timestamp\": " + date + ", " + "\"status\": 401, " + "\"error\": \"Não autorizado\", "
+				+ "\"message\": \"Email ou senha inválidos\", " + "\"path\": \"/login\"}";
+	}
+}
